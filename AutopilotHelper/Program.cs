9d877da2cce5f2e4f253976f9fab3193c3300e65
Shepherd Zhu <shepherd0619@outlook.com>
2024-12-23 12:24:23 +0800

    [feat] check adk installation during startup

--- AutopilotHelper/Program.cs
+++ AutopilotHelper/Program.cs
@@ -1,3 +1,6 @@
+using AutopilotHelper.Utilities;
+using System.Diagnostics;
+
 namespace AutopilotHelper
 {
     internal static class Program
@@ -11,6 +14,22 @@ namespace AutopilotHelper
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
+            if (string.IsNullOrEmpty(AdkUtil.GetAdkPath()))
+            {
+                var result = MessageBox.Show("It appears the Windows ADK is not installed.\n" +
+                    "If you wish to decode hardware hash, ADK is required.\n\n" +
+                    "Head to Microsoft Learn for download?", "Windows ADK is missing", MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+
+                if(result == DialogResult.OK)
+                {
+                    ProcessStartInfo startInfo = new ProcessStartInfo();
+                    startInfo.FileName = "https://learn.microsoft.com/en-us/windows-hardware/get-started/adk-install"; // Õ¯“≥µÿ÷∑
+                    startInfo.UseShellExecute = true;
+
+                    Process process = Process.Start(startInfo);
+                }
+            }
+
             Application.Run(new StartUpForm());
         }
     }
