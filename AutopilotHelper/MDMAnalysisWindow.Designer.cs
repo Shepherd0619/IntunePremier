8d499fab03fa4564a017028f13bd1237a9515218
Shepherd Zhu <shepherd0619@outlook.com>
2025-01-08 23:00:56 +0800

    [feat] ESP tab page

--- AutopilotHelper/MDMAnalysisWindow.Designer.cs
+++ AutopilotHelper/MDMAnalysisWindow.Designer.cs
@@ -45,15 +45,35 @@
             label4 = new Label();
             label1 = new Label();
             progressBar1 = new ProgressBar();
+            EspPage = new TabPage();
+            panel3 = new Panel();
+            label7 = new Label();
+            accountSetupCheckedListBox = new CheckedListBox();
+            panel2 = new Panel();
+            label6 = new Label();
+            deviceSetupCheckedListBox = new CheckedListBox();
+            panel1 = new Panel();
+            label3 = new Label();
+            devicePreparationCheckedListBox = new CheckedListBox();
+            label8 = new Label();
+            label2 = new Label();
             OOBETab = new TabPage();
             ProcessedPoliciesTab = new TabPage();
             ProcessedPoliciesWebView = new Microsoft.Web.WebView2.WinForms.WebView2();
+            ProcessedAppsTab = new TabPage();
+            ProcessedAppsWebView = new Microsoft.Web.WebView2.WinForms.WebView2();
             menuStrip1.SuspendLayout();
             tabControl1.SuspendLayout();
             AutopilotDiagTab.SuspendLayout();
             ProfileTab.SuspendLayout();
+            EspPage.SuspendLayout();
+            panel3.SuspendLayout();
+            panel2.SuspendLayout();
+            panel1.SuspendLayout();
             ProcessedPoliciesTab.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)ProcessedPoliciesWebView).BeginInit();
+            ProcessedAppsTab.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)ProcessedAppsWebView).BeginInit();
             SuspendLayout();
             // 
             // menuStrip1
@@ -108,7 +128,7 @@
             // aboutToolStripMenuItem
             // 
             aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
-            aboutToolStripMenuItem.Size = new Size(180, 22);
+            aboutToolStripMenuItem.Size = new Size(107, 22);
             aboutToolStripMenuItem.Text = "About";
             aboutToolStripMenuItem.Click += aboutToolStripMenuItem_Click;
             // 
@@ -116,13 +136,15 @@
             // 
             tabControl1.Controls.Add(AutopilotDiagTab);
             tabControl1.Controls.Add(ProfileTab);
+            tabControl1.Controls.Add(EspPage);
             tabControl1.Controls.Add(OOBETab);
             tabControl1.Controls.Add(ProcessedPoliciesTab);
+            tabControl1.Controls.Add(ProcessedAppsTab);
             tabControl1.Dock = DockStyle.Fill;
             tabControl1.Location = new Point(0, 24);
             tabControl1.Name = "tabControl1";
             tabControl1.SelectedIndex = 0;
-            tabControl1.Size = new Size(800, 426);
+            tabControl1.Size = new Size(800, 510);
             tabControl1.TabIndex = 1;
             // 
             // AutopilotDiagTab
@@ -130,7 +152,7 @@
             AutopilotDiagTab.Controls.Add(autopilotDiagTextBox1);
             AutopilotDiagTab.Location = new Point(4, 24);
             AutopilotDiagTab.Name = "AutopilotDiagTab";
-            AutopilotDiagTab.Size = new Size(792, 398);
+            AutopilotDiagTab.Size = new Size(792, 482);
             AutopilotDiagTab.TabIndex = 2;
             AutopilotDiagTab.Text = "Diagnostics";
             AutopilotDiagTab.UseVisualStyleBackColor = true;
@@ -142,7 +164,7 @@
             autopilotDiagTextBox1.Name = "autopilotDiagTextBox1";
             autopilotDiagTextBox1.ReadOnly = true;
             autopilotDiagTextBox1.ScrollBars = RichTextBoxScrollBars.ForcedVertical;
-            autopilotDiagTextBox1.Size = new Size(792, 398);
+            autopilotDiagTextBox1.Size = new Size(792, 482);
             autopilotDiagTextBox1.TabIndex = 0;
             autopilotDiagTextBox1.Text = "";
             // 
@@ -156,7 +178,7 @@
             ProfileTab.Location = new Point(4, 24);
             ProfileTab.Name = "ProfileTab";
             ProfileTab.Padding = new Padding(3);
-            ProfileTab.Size = new Size(792, 398);
+            ProfileTab.Size = new Size(792, 482);
             ProfileTab.TabIndex = 0;
             ProfileTab.Text = "Profile";
             ProfileTab.UseVisualStyleBackColor = true;
@@ -209,12 +231,132 @@
             progressBar1.Size = new Size(776, 23);
             progressBar1.TabIndex = 0;
             // 
+            // EspPage
+            // 
+            EspPage.Controls.Add(panel3);
+            EspPage.Controls.Add(panel2);
+            EspPage.Controls.Add(panel1);
+            EspPage.Controls.Add(label8);
+            EspPage.Controls.Add(label2);
+            EspPage.Location = new Point(4, 24);
+            EspPage.Name = "EspPage";
+            EspPage.Size = new Size(792, 482);
+            EspPage.TabIndex = 5;
+            EspPage.Text = "ESP";
+            EspPage.UseVisualStyleBackColor = true;
+            // 
+            // panel3
+            // 
+            panel3.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            panel3.Controls.Add(label7);
+            panel3.Controls.Add(accountSetupCheckedListBox);
+            panel3.Location = new Point(8, 339);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(776, 128);
+            panel3.TabIndex = 10;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label7.Location = new Point(0, 0);
+            label7.Name = "label7";
+            label7.Size = new Size(110, 20);
+            label7.TabIndex = 6;
+            label7.Text = "Account setup";
+            // 
+            // accountSetupCheckedListBox
+            // 
+            accountSetupCheckedListBox.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            accountSetupCheckedListBox.FormattingEnabled = true;
+            accountSetupCheckedListBox.Location = new Point(0, 34);
+            accountSetupCheckedListBox.Name = "accountSetupCheckedListBox";
+            accountSetupCheckedListBox.Size = new Size(776, 94);
+            accountSetupCheckedListBox.TabIndex = 5;
+            // 
+            // panel2
+            // 
+            panel2.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            panel2.Controls.Add(label6);
+            panel2.Controls.Add(deviceSetupCheckedListBox);
+            panel2.Location = new Point(8, 205);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(776, 131);
+            panel2.TabIndex = 9;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label6.Location = new Point(0, 0);
+            label6.Name = "label6";
+            label6.Size = new Size(98, 20);
+            label6.TabIndex = 4;
+            label6.Text = "Device setup";
+            // 
+            // deviceSetupCheckedListBox
+            // 
+            deviceSetupCheckedListBox.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            deviceSetupCheckedListBox.FormattingEnabled = true;
+            deviceSetupCheckedListBox.Location = new Point(0, 37);
+            deviceSetupCheckedListBox.Name = "deviceSetupCheckedListBox";
+            deviceSetupCheckedListBox.Size = new Size(776, 94);
+            deviceSetupCheckedListBox.TabIndex = 3;
+            // 
+            // panel1
+            // 
+            panel1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            panel1.Controls.Add(label3);
+            panel1.Controls.Add(devicePreparationCheckedListBox);
+            panel1.Location = new Point(8, 71);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(776, 128);
+            panel1.TabIndex = 8;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.Location = new Point(0, 0);
+            label3.Name = "label3";
+            label3.Size = new Size(141, 20);
+            label3.TabIndex = 2;
+            label3.Text = "Device preparation";
+            // 
+            // devicePreparationCheckedListBox
+            // 
+            devicePreparationCheckedListBox.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            devicePreparationCheckedListBox.FormattingEnabled = true;
+            devicePreparationCheckedListBox.Location = new Point(0, 34);
+            devicePreparationCheckedListBox.Name = "devicePreparationCheckedListBox";
+            devicePreparationCheckedListBox.Size = new Size(776, 94);
+            devicePreparationCheckedListBox.TabIndex = 1;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(8, 36);
+            label8.Name = "label8";
+            label8.Size = new Size(310, 15);
+            label8.TabIndex = 7;
+            label8.Text = "* for detailed information, please head to Diagnostics tab.";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(8, 11);
+            label2.Name = "label2";
+            label2.Size = new Size(275, 25);
+            label2.TabIndex = 0;
+            label2.Text = "Setting up for work or school";
+            // 
             // OOBETab
             // 
             OOBETab.Location = new Point(4, 24);
             OOBETab.Name = "OOBETab";
             OOBETab.Padding = new Padding(3);
-            OOBETab.Size = new Size(792, 398);
+            OOBETab.Size = new Size(792, 482);
             OOBETab.TabIndex = 1;
             OOBETab.Text = "OOBE";
             OOBETab.UseVisualStyleBackColor = true;
@@ -224,7 +366,7 @@
             ProcessedPoliciesTab.Controls.Add(ProcessedPoliciesWebView);
             ProcessedPoliciesTab.Location = new Point(4, 24);
             ProcessedPoliciesTab.Name = "ProcessedPoliciesTab";
-            ProcessedPoliciesTab.Size = new Size(792, 398);
+            ProcessedPoliciesTab.Size = new Size(792, 482);
             ProcessedPoliciesTab.TabIndex = 3;
             ProcessedPoliciesTab.Text = "Processed Policies";
             ProcessedPoliciesTab.UseVisualStyleBackColor = true;
@@ -237,16 +379,38 @@
             ProcessedPoliciesWebView.Dock = DockStyle.Fill;
             ProcessedPoliciesWebView.Location = new Point(0, 0);
             ProcessedPoliciesWebView.Name = "ProcessedPoliciesWebView";
-            ProcessedPoliciesWebView.Size = new Size(792, 398);
+            ProcessedPoliciesWebView.Size = new Size(792, 482);
             ProcessedPoliciesWebView.TabIndex = 0;
             ProcessedPoliciesWebView.ZoomFactor = 1D;
+            // 
+            // ProcessedAppsTab
+            // 
+            ProcessedAppsTab.Controls.Add(ProcessedAppsWebView);
+            ProcessedAppsTab.Location = new Point(4, 24);
+            ProcessedAppsTab.Name = "ProcessedAppsTab";
+            ProcessedAppsTab.Size = new Size(792, 482);
+            ProcessedAppsTab.TabIndex = 4;
+            ProcessedAppsTab.Text = "Processed Apps";
+            ProcessedAppsTab.UseVisualStyleBackColor = true;
+            // 
+            // ProcessedAppsWebView
+            // 
+            ProcessedAppsWebView.AllowExternalDrop = true;
+            ProcessedAppsWebView.CreationProperties = null;
+            ProcessedAppsWebView.DefaultBackgroundColor = Color.White;
+            ProcessedAppsWebView.Dock = DockStyle.Fill;
+            ProcessedAppsWebView.Location = new Point(0, 0);
+            ProcessedAppsWebView.Name = "ProcessedAppsWebView";
+            ProcessedAppsWebView.Size = new Size(792, 482);
+            ProcessedAppsWebView.TabIndex = 0;
+            ProcessedAppsWebView.ZoomFactor = 1D;
             // 
             // MDMAnalysisWindow
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             AutoSize = true;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(800, 534);
             Controls.Add(tabControl1);
             Controls.Add(menuStrip1);
             MainMenuStrip = menuStrip1;
@@ -261,8 +425,18 @@
             AutopilotDiagTab.ResumeLayout(false);
             ProfileTab.ResumeLayout(false);
             ProfileTab.PerformLayout();
+            EspPage.ResumeLayout(false);
+            EspPage.PerformLayout();
+            panel3.ResumeLayout(false);
+            panel3.PerformLayout();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
             ProcessedPoliciesTab.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)ProcessedPoliciesWebView).EndInit();
+            ProcessedAppsTab.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)ProcessedAppsWebView).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -289,5 +463,19 @@
         private Microsoft.Web.WebView2.WinForms.WebView2 ProcessedPoliciesWebView;
         private ToolStripSeparator toolStripSeparator1;
         private ToolStripMenuItem openWorkspaceFolderToolStripMenuItem;
+        private TabPage ProcessedAppsTab;
+        private Microsoft.Web.WebView2.WinForms.WebView2 ProcessedAppsWebView;
+        private TabPage EspPage;
+        private Label label2;
+        private Label label7;
+        private CheckedListBox accountSetupCheckedListBox;
+        private Label label6;
+        private CheckedListBox deviceSetupCheckedListBox;
+        private Label label3;
+        private CheckedListBox devicePreparationCheckedListBox;
+        private Label label8;
+        private Panel panel1;
+        private Panel panel3;
+        private Panel panel2;
     }
 }
--- AutopilotHelper/MDMAnalysisWindow.cs
+++ AutopilotHelper/MDMAnalysisWindow.cs
@@ -1,4 +1,6 @@
-﻿using AutopilotHelper.Utilities;
+﻿using AutopilotHelper.Models;
+using AutopilotHelper.Utilities;
+using Newtonsoft.Json;
 using System.Diagnostics;
 
 namespace AutopilotHelper
@@ -45,11 +47,96 @@ namespace AutopilotHelper
             }
             _autopilotUtil.GetCloudSessionHostRecords();
 
+            try
+            {
+                InitializeESPTabPage(_autopilotUtil.GetAutopilotSettingsFromRegistry());
+            }
+            catch(Exception ex)
+            {
+                // Do nothing
+            }
+
             autopilotDiagTextBox1.Text = _autopilotUtil.GetGeneralDiagnosticsReport();
 
             await ProcessedPoliciesWebView.EnsureCoreWebView2Async();
 
             ProcessedPoliciesWebView.NavigateToString(_autopilotUtil.GetHtmlFormattedProcessedPolicies());
+
+            await ProcessedAppsWebView.EnsureCoreWebView2Async();
+        }
+
+        public void InitializeESPTabPage(AutopilotSettings settings)
+        {
+            devicePreparationCheckedListBox.Items.Clear();
+            deviceSetupCheckedListBox.Items.Clear();
+            accountSetupCheckedListBox.Items.Clear();
+
+            try
+            {
+                var esp = _autopilotUtil.GetAutopilotSettingsFromRegistry();
+
+                try
+                {
+                    var devicePreparation = JsonConvert.DeserializeObject<DevicePreparationCategory>(esp.DevicePreparation.Status);
+                    foreach (var item in devicePreparation.GetType().GetProperties())
+                    {
+                        if (item.PropertyType != typeof(Subcategory)) continue;
+
+                        var category = item.GetValue(devicePreparation) as Subcategory;
+
+                        devicePreparationCheckedListBox.Items.Add(item.Name, 
+                            category.SubcategoryState == "succeeded");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    // Do nothing
+                }
+
+                try
+                {
+                    var deviceSetup = JsonConvert.DeserializeObject<DeviceSetupCategory>(esp.DeviceSetup.Status);
+                    foreach (var item in deviceSetup.GetType().GetProperties())
+                    {
+                        if (item.PropertyType != typeof(Subcategory)) continue;
+
+                        var category = item.GetValue(deviceSetup) as Subcategory;
+
+                        deviceSetupCheckedListBox.Items.Add($"{item.Name} - {category.SubcategoryStatusText}",
+                            category.SubcategoryState == "succeeded");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    // Do nothing
+                }
+
+                try
+                {
+                    var accountSetup = JsonConvert.DeserializeObject<AccountSetupCategory>(esp.AccountSetup.Status);
+                    foreach (var item in accountSetup.GetType().GetProperties())
+                    {
+                        if (item.PropertyType != typeof(Subcategory)) continue;
+
+                        var category = item.GetValue(accountSetup) as Subcategory;
+
+                        accountSetupCheckedListBox.Items.Add(item.Name,
+                            category.SubcategoryState == "succeeded");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    // Do nothing
+                }
+
+                devicePreparationCheckedListBox.ItemCheck += ESPTabPageCheckedListBox_ItemCheck;
+                deviceSetupCheckedListBox.ItemCheck += ESPTabPageCheckedListBox_ItemCheck;
+                accountSetupCheckedListBox.ItemCheck += ESPTabPageCheckedListBox_ItemCheck;
+            }
+            catch (Exception ex)
+            {
+                // Do nothing
+            }
         }
 
         private void MDMAnalysisWindow_FormClosed(object sender, FormClosedEventArgs e)
@@ -86,6 +173,22 @@ namespace AutopilotHelper
             startInfo.UseShellExecute = true;
 
             Process process = Process.Start(startInfo);
+        }
+
+        private void ESPTabPageCheckedListBox_ItemCheck(object sender, ItemCheckEventArgs e)
+        {
+            // 阻止用户更改项目状态
+            if (e.NewValue == CheckState.Checked)
+            {
+                e.NewValue = CheckState.Unchecked;
+                return;
+            }
+
+            if (e.NewValue == CheckState.Unchecked)
+            {
+                e.NewValue = CheckState.Checked;
+                return;
+            }
         }
     }
 }
