86c16905d4000828f8e8fe897d840f26d79d71de
Shepherd Zhu <shepherd0619@outlook.com>
2025-03-07 21:59:29 +0800

    [feat] ChatGPTConst & ChatGPTController

---
+++ AutopilotHelper/Const/ChatGPTConst.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutopilotHelper.Const
+{
+    public static class ChatGPTConst
+    {
+        public static class ConversationId
+        {
+            public const string Undefined = "Undefined";
+            public const string EventViewer = "EventViewer";
+            public const string RegViewer = "RegViewer";
+        }
+
+        public static class Role
+        {
+            public const string System = "system";
+            public const string User = "user";
+        }
+
+        public static class Prompt
+        {
+            public const string SupportEngineer = "You are Microsoft Intune Support Engineer.";
+            public const string EventVIewer = "You are Microsoft Intune Support Engineer. " +
+                "I will provide a Event Viewer log list. You should answer my questions based on Event Viewer log and Microsoft Learn documentation.";
+            public const string RegViewer = "You are Microsoft Intune Support Engineer. " +
+                "I will provide a registry body. You should answer my questions based on registry and Microsoft Learn documentation.";
+        }
+    }
+}
---
+++ AutopilotHelper/Controllers/ChatGPTController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutopilotHelper.Const;
+using ChatGPT.Net;
+
+namespace AutopilotHelper.Controllers
+{
+    public static class ChatGPTController
+    {
+        public static ChatGpt Api { get; set; }
+
+        public static async Task<bool> RegeneratePreviousMsg(this ChatGpt api, string conversationId)
+        {
+            var conversation = api.GetConversation(conversationId);
+            if(conversation == null)
+            {
+                return false;
+            }
+
+            var messages = conversation.Messages;
+            if(messages == null || messages.Count <= 0)
+            {
+                return false;
+            }
+
+            var lastMessage = messages.Last();
+
+            if(lastMessage == null || lastMessage.Role != ChatGPTConst.Role.System)
+            {
+                return false;
+            }
+
+            messages.Remove(lastMessage);
+
+
+
+            return true;
+        }
+    }
+}
